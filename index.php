10763fa47de123d8f7a0ed648fa12c743ca6eb55
gianluca <messinagianluca66@gmail.com>
2020-05-24 15:26:53 +0200

    headerV1

--- index.php
+++ index.php
@@ -4,8 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Covid-19Analytics</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- ESEMPIO DELL'HEADER COME VA FATTO -->
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
-<body>
-    
+<body>		
+	<!-- left side  -->
+<header>
+<div class="w3-top">
+	<div class="w3-bar w3-white w3-card" id="myNavbar">
+		<a href="#home" class="w3-bar-item w3-button w3-wide">LOGO</a>
+		<a href="#news" class="w3-bar-item w3-button w3-wide">NEWS</a>
+	
+    <!-- right side-->
+	<div class="w3-right w3-hide-small">
+      <a href="#signup" class="w3-bar-item w3-button"><i class="fa fa-th"></i> SIGNUP</a>
+      <a href="#login" class="w3-bar-item w3-button"><i class="fa fa-usd"></i> LOGIN</a>
+      <a href="#contact" class="w3-bar-item w3-button"><i class="fa fa-envelope"></i> CONTACT</a>
+	</div>
+	</div>
+</div>
+</header>
 </body>
 </html>
