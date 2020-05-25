6f40da018202e4aca9fe68f8452dff713a97f5bc
gianluca <messinagianluca66@gmail.com>
2020-05-25 12:54:22 +0200

    headerV2

--- index.php
+++ index.php
@@ -4,29 +4,64 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Covid-19Analytics</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
+	<meta name="viewport" content="width=device-width, initial-scale=1">	
 	<!-- ESEMPIO DELL'HEADER COME VA FATTO -->
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="./css/main.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
-<body>		
-	<!-- left side  -->
-<header>
-<div class="w3-top">
-	<div class="w3-bar w3-white w3-card" id="myNavbar">
-		<a href="#home" class="w3-bar-item w3-button w3-wide">LOGO</a>
-		<a href="#news" class="w3-bar-item w3-button w3-wide">NEWS</a>
-	
-    <!-- right side-->
-	<div class="w3-right w3-hide-small">
-      <a href="#signup" class="w3-bar-item w3-button"><i class="fa fa-th"></i> SIGNUP</a>
-      <a href="#login" class="w3-bar-item w3-button"><i class="fa fa-usd"></i> LOGIN</a>
-      <a href="#contact" class="w3-bar-item w3-button"><i class="fa fa-envelope"></i> CONTACT</a>
-	</div>
-	</div>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#343791">
+  <a class="navbar-brand" href="#"><img src="" width="30" height="30" alt="logo" loading="lazy"></img></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <!-- collapse riduce ad un bottone se la dimensione scende sotto una soglia -->
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">News</a>
+      </li>
+    </ul>
+		<form class="form-inline my-2 my-lg-0">
+		<ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Login</a>
+      </li>
+	  <li class="nav-item">
+		 <a class="nav-link" href="#">Sign up</a>
+	  </li>
+	   <li class="nav-item">
+		 <a class="nav-link" href="#">Contact</a>
+	  </li>
+    </ul>
+    </form>
+  </div>
+</nav>
+<div class="jumbotron jumbotron-fluid" style="background-color:#ededed" >
+  <div class="container">
+   <img src="./img/prev1.png" alt="..." width="300" height="170" class="rounded float-left display-4 .img-fluid. max-width: 100%;"> 
+    <h1 class="">REGISTRATI ORA!!</h1>
+    <p class="lead">Solo per te ci sono delle previsioni appena uscite!</p>
+  </div>
 </div>
-</header>
+
+
+
+<div>
+</div>
+
+
+
+<footer class="text-muted">
+  <div class="container">
+    <p class="float-right">
+      <a href="#">Back to top</a>
+    </p>
+    <p>hai visitato il sito per tenerti aggiornato sul corona grazie!!</p>
+  </div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 </body>
 </html>
